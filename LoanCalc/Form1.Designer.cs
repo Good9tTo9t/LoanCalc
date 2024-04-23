067351667d8b5e410a382f438d4e12623f39d6a9
Hiro Wu <10113210@gm.ptivs.tn.edu.tw>
2024-04-23 23:18:59 +0800

    made some mistakes lol

--- LoanCalc/Form1.Designer.cs
+++ LoanCalc/Form1.Designer.cs
@@ -29,59 +29,58 @@ namespace LoanCalc
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(211, 272);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(419, 141);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "本息平均攤還法";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(211, 272);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(419, 141);
+            button1.TabIndex = 0;
+            button1.Text = "本息平均攤還法";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(662, 272);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(419, 141);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "本金平均攤還法";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            button2.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(662, 272);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(419, 141);
+            button2.TabIndex = 1;
+            button2.Text = "本金平均攤還法";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(371, 135);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(551, 122);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Mortgage Calculator\r\nby Wu, I-Hung 113210\r\n";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(371, 135);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(551, 122);
+            label1.TabIndex = 2;
+            label1.Text = "Mortgage Calculator\r\nby Wu, I-Hung 113210\r\n";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1264, 681);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
